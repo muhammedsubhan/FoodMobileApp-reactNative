ff3d8a54309d1cdc3b76aea34d3aac70db6330be
Subhan <muhammedsubhan692@gmail.com>
2023-12-04 11:35:45 +0500

    food Categories Screen

---
+++ dummyData/dummy-data.js
@@ -0,0 +1,34 @@
+export const Category = [
+  {
+    id: 1,
+    name: "Pakistani Food",
+  },
+  {
+    id: 2,
+    name: "Indian Food",
+  },
+  {
+    id: 3,
+    name: "Italian  Food",
+  },
+  {
+    id: 4,
+    name: "French Food",
+  },
+  {
+    id: 5,
+    name: "Japanese Food",
+  },
+  {
+    id: 6,
+    name: "American Food",
+  },
+  {
+    id: 7,
+    name: "Turkey Food",
+  },
+  {
+    id: 8,
+    name: "Thai  Food",
+  },
+];
